dfa4db217e864a57919407910cb428983bf8e230
Francisco Martín Rico <fmrico@gmail.com>
2024-09-18 10:46:17 +0200

    Add EndToEnd trace
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- exercise3-2/solutions/application_nodes.cc
+++ exercise3-2/solutions/application_nodes.cc
@@ -18,22 +18,16 @@ CameraProcessingNode::CameraProcessingNode(
   std::shared_ptr<ThreadTracer> tracer_object_detector,
   std::shared_ptr<ThreadTracer> tracer_data_logger
 ) : Node("obj_detect"), tracer_object_detector_(tracer_object_detector), tracer_data_logger_(tracer_data_logger) {
-  callback_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
-  rclcpp::SubscriptionOptions subscription_options;
-  subscription_options.callback_group = callback_group_;
-
   subscription_data_logger_ = this->create_subscription<FakeImage>(
     "/image",
     10,
-    std::bind(&CameraProcessingNode::DataLoggerCallback, this, std::placeholders::_1),
-    subscription_options
+    std::bind(&CameraProcessingNode::DataLoggerCallback, this, std::placeholders::_1)
   );
 
   subscription_object_detector_ = this->create_subscription<FakeImage>(
     "/image",
     10,
-    std::bind(&CameraProcessingNode::ObjectDetectorCallback, this, std::placeholders::_1),
-    subscription_options
+    std::bind(&CameraProcessingNode::ObjectDetectorCallback, this, std::placeholders::_1)
   );
 
   publisher_ = this->create_publisher<std_msgs::msg::Int64>("/actuation", 10);
@@ -52,8 +46,8 @@ void CameraProcessingNode::ObjectDetectorCallback(const FakeImage::SharedPtr ima
   {
     auto span = tracer_object_detector_->WithSpan("ObjectDetect");
 
-    // Pretend it takes 3ms to do object detection.
-    WasteTime(std::chrono::microseconds(3000));
+    // Pretend it takes 4000 ms to do object detection.
+    WasteTime(std::chrono::microseconds(4000));
 
     // Send a signal to the downstream actuation node
     std_msgs::msg::Int64 msg;
@@ -71,14 +65,8 @@ void CameraProcessingNode::DataLoggerCallback(const FakeImage::SharedPtr image) 
   {
     auto span = tracer_data_logger_->WithSpan("DataLogger");
 
-    // Generate random value between 1000 and 10000
-    int random_value = 0;
-    for (int i = 0; i < 10; i++) {
-      random_value += (rand() % 900) + 100;
-    }
-
-    // Assume it takes 6ms to serialize the data which is all on the CPU
-    WasteTime(std::chrono::microseconds(random_value));
+    // Assume it takes 1ms to serialize the data which is all on the CPU
+    WasteTime(std::chrono::microseconds(1000));
 
     // Assume it takes about 1ms to write the data where it is blocking but yielded to the CPU.
     std::this_thread::sleep_for(std::chrono::microseconds(1000));
@@ -94,7 +82,6 @@ ActuationNode::ActuationNode(
     std::bind(&ActuationNode::MessageCallback, this, std::placeholders::_1)
   );
 
-  // Set actuation node to high priority
   sched_param sp;
   sp.sched_priority = HIGH;
   subscription_->sched_param(sp);
